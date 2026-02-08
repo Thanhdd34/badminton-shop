fdf3e1fca8b9f8ae948575ed22d567f01f2e26a3
Dang Thanh <thanhdd34@gmail.com>
2026-02-09 00:09:11 +0700

    add user entity

---
+++ src/main/java/com/thanhdd34/badmintonshop/entity/User.java
@@ -0,0 +1,30 @@
+package com.thanhdd34.badmintonshop.entity;
+
+import com.thanhdd34.badmintonshop.entity.enums.Role;
+import jakarta.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name="created_at")
+    private LocalDateTime createdAt;
+}
+
---
+++ src/main/java/com/thanhdd34/badmintonshop/entity/enums/Role.java
@@ -0,0 +1,9 @@
+package com.thanhdd34.badmintonshop.entity.enums;
+
+
+import java.time.LocalDateTime;
+
+public enum Role {
+    USER,
+    ADMIN
+}
