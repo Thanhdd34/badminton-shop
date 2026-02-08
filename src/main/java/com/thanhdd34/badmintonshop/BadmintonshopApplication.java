c1827f4ae8ddb60fce0ccc0f937d028d38b17469
Dang Thanh <thanhdd34@gmail.com>
2026-02-08 23:38:45 +0700

    init spring boot project - badmintonSHop e-commerce

---
+++ src/main/java/com/thanhdd34/badmintonshop/BadmintonshopApplication.java
@@ -0,0 +1,13 @@
+package com.thanhdd34.badmintonshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadmintonshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadmintonshopApplication.class, args);
+	}
+
+}
